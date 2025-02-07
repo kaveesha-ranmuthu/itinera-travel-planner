4e3a4e3ef4bef135eff47abfe222c01cc05bf1bd
Kaveesha Ranmuthu <kranmuthu1999@gmail.com>
2025-02-07 14:44:55 +1300

    add logo to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import "./App.css";
+import art1 from "./assets/art-1.jpg";
+import title from "./assets/title.svg";
 
 function App() {
   return (
-    <div className="bg-[url(./assets/art-1.jpg)] w-dvw h-dvh bg-cover opacity-50"></div>
+    <div className=" w-dvw h-dvh bg-cover bg-black">
+      <img src={art1} className="w-full h-full opacity-50" />
+      <div className="absolute bottom-5 left-10 flex items-center justify-center w-full h-full text-primary">
+        <img src={title} alt="itinera travel planner" />
+      </div>
+    </div>
   );
 }
 
