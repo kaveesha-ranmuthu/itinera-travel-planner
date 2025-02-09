704e4215ee47ea647681ac5d49542f412f739cc3
Kaveesha Ranmuthu <kranmuthu1999@gmail.com>
2025-02-09 21:58:19 +1300

    add loading page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import LandingPage from "./pages/landing-page/LandingPage";
+import LandingPage, { LoadingState } from "./pages/landing-page/LandingPage";
 import LoginPage from "./pages/authentication/LoginPage";
 import SignupPage from "./pages/authentication/SignupPage";
 import { auth } from "./firebase-config";
@@ -10,10 +10,19 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [user, setUser] = useState<null | User>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    const MIN_LOADING_TIME = 3000;
+    const startTime = Date.now();
+
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+
+      const elapsedTime = Date.now() - startTime;
+      const remainingTime = Math.max(0, MIN_LOADING_TIME - elapsedTime);
+
+      setTimeout(() => setLoading(false), remainingTime);
     });
 
     return () => unsubscribe();
@@ -24,7 +33,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={user ? <TripsPage /> : <LandingPage />} />
+        <Route
+          path="/"
+          element={
+            loading ? <LoadingState /> : user ? <TripsPage /> : <LandingPage />
+          }
+        />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
--- src/pages/landing-page/LandingPage.tsx
+++ src/pages/landing-page/LandingPage.tsx
@@ -22,4 +22,16 @@ const LandingPage = () => {
   );
 };
 
+export const LoadingState = () => {
+  return (
+    <BackgroundWrapper>
+      <div className="absolute bottom-5 left-0 flex items-center justify-center w-full h-full">
+        <div className="text-center animate-pulse">
+          <Logo marginLeft="ml-24" />
+        </div>
+      </div>
+    </BackgroundWrapper>
+  );
+};
+
 export default LandingPage;
