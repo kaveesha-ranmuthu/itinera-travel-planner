dba691dc420a666ab94cdccb6dae27f6173660af
Kaveesha Ranmuthu <kranmuthu1999@gmail.com>
2025-02-09 22:06:36 +1300

    temp logout button

--- src/pages/trips-page/TripsPage.tsx
+++ src/pages/trips-page/TripsPage.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import Button from "../../components/Button";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase-config";
+import { useHotToast } from "../../hooks/useHotToast";
+import { useNavigate } from "react-router-dom";
 
 const TripsPage = () => {
-  return <div>Trips</div>;
+  const { notify } = useHotToast();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/login");
+    } catch {
+      notify("Something went wrong. Please try again.", "error");
+    }
+  };
+
+  return (
+    <div className="p-10">
+      <Button.Secondary onClick={handleLogout}>Log out</Button.Secondary>
+    </div>
+  );
 };
 
 export default TripsPage;
