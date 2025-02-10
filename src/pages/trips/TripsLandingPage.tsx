992c557b710a3053a958324bc5da378ec158d7fd
Kaveesha Ranmuthu <kranmuthu1999@gmail.com>
2025-02-10 20:18:58 +1300

    add create new trip button

--- src/pages/trips/TripsLandingPage.tsx
+++ src/pages/trips/TripsLandingPage.tsx
@@ -8,10 +8,19 @@ const TripsLandingPage = () => {
   return (
     <div className={settings?.font ?? FontFamily.HANDWRITTEN}>
       <Header />
-      <div className="flex justify-center text-4xl tracking-widest">
+      <div className="flex justify-center text-5xl tracking-widest">
         <h1>my trips</h1>
       </div>
+      <CreateNewTripButton />
     </div>
+  );
+};
+
+const CreateNewTripButton = () => {
+  return (
+    <button className="hover:opacity-60 transition ease-in-out duration-400 cursor-pointer border border-secondary w-96 rounded-2xl h-56 flex items-center justify-center drop-shadow-(--drop-shadow-default)">
+      <p className="text-2xl">Create new trip</p>
+    </button>
   );
 };
 
