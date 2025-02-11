7b834df5cad9710adf8b57ea5a5d9a86782c6bee
Kaveesha Ranmuthu <kranmuthu1999@gmail.com>
2025-02-11 21:41:35 +1300

    upload all images

---
+++ src/pages/trips/images/images.ts
@@ -0,0 +1,5 @@
+import { images as janBrueghelTheYoungerImages } from "./jan-brueghel-the-younger";
+
+export const images = [
+  { artist: "Jan Brueghel the Younger", images: janBrueghelTheYoungerImages },
+];
