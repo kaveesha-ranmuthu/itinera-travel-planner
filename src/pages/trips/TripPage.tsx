a5776e7940c5fe75640a3ca262fa5cf678d7b805
Kaveesha Ranmuthu <kranmuthu1999@gmail.com>
2025-02-14 13:02:02 +1300

    header image and title

--- src/pages/trips/TripPage.tsx
+++ src/pages/trips/TripPage.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import useGetTrip from "./hooks/getters/useGetTrip";
 import ErrorPage from "../error/ErrorPage";
 import { LoadingState } from "../landing-page/LandingPage";
+import Header from "./components/Header";
+import { useAuth } from "../../hooks/useAuth";
 
 const TripPage = () => {
   const { tripId } = useParams();
@@ -19,16 +21,35 @@ interface TripInfoProps {
 }
 const TripInfo: React.FC<TripInfoProps> = ({ tripId }) => {
   const { error, loading, trip } = useGetTrip(tripId);
+  const { settings } = useAuth();
 
   if (loading) {
     return <LoadingState />;
   }
 
-  if (error) {
+  if (error || !trip) {
     return <ErrorPage />;
   }
 
-  console.log(trip);
+  const { tripName, imageData } = trip;
+
+  return (
+    <div className={settings?.font}>
+      <Header />
+      <div className="px-10">
+        <div className="bg-black rounded-3xl relative">
+          <img
+            src={imageData}
+            alt={tripName}
+            className="object-cover w-full h-[311px] rounded-3xl opacity-50"
+          />
+          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+            <p className="text-primary text-5xl">{tripName}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TripPage;
