8ee4283b4a5fb1a12cc63c55d5e6b8dd9aaec99c
Kaveesha Ranmuthu <kranmuthu1999@gmail.com>
2025-02-24 15:57:29 +1300

    create table component with subcomponent

--- src/pages/trips/components/Table.tsx
+++ src/pages/trips/components/Table.tsx
@@ -1,95 +1,58 @@
+import { PropsWithChildren } from "react";
 import { twMerge } from "tailwind-merge";
-import Checkbox from "./Checkbox";
-import { useEffect, useState } from "react";
-import { GoCopy } from "react-icons/go";
-import { IoTrashBinOutline } from "react-icons/io5";
 
 export type Data = {
   components: React.ReactNode[];
   checked: boolean;
 };
 
-interface TableProps {
-  headers: string[];
-  data: Data[];
-  checkedColor: string;
+interface Props {
+  className?: string;
 }
 
-const Table: React.FC<TableProps> = ({ checkedColor, headers, data }) => {
-  const [allChecked, setAllChecked] = useState(false);
-
-  useEffect(() => {
-    const allChecked = data.every((row) => row.checked);
-    setAllChecked(allChecked);
-  }, [data]);
-
+const TableWrapper: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <table className="text-secondary w-full border border-secondary rounded-2xl border-separate border-spacing-0 ">
-      <thead className="font-normal">
-        <tr>
-          <td className="border-b border-r py-2 px-4">
-            <div className="flex items-center space-x-4">
-              <Checkbox checked={allChecked} />
-              <span>{headers[0]}</span>
-            </div>
-          </td>
-          {headers.slice(1).map((header, index) => (
-            <td
-              key={index}
-              className="border-b pl-10 pr-4 border-r last:border-r-0 border-secondary py-2"
-            >
-              {header}
-            </td>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, index) => {
-          return (
-            <tr key={index} className="group">
-              <td
-                className={twMerge(
-                  "group-last:border-b-0 border-b border-r py-2 px-4 group-last:rounded-bl-2xl",
-                  row.checked ? checkedColor : ""
-                )}
-              >
-                <div className="flex items-center space-x-4">
-                  <span>
-                    <Checkbox checked={row.checked} />
-                  </span>
-                  <span className="w-full">{row.components[0]}</span>
-                </div>
-              </td>
-              {row.components.slice(1).map((component, index) => (
-                <td
-                  key={index}
-                  className={twMerge(
-                    "group-last:border-b-0 border-b pl-10 pr-4 border-r last:border-r-0 border-secondary py-2",
-                    row.checked ? checkedColor : ""
-                  )}
-                >
-                  {component}
-                </td>
-              ))}
-              <td className="group-last:border-b-0 px-1 rounded-br-2xl border-b border-r last:border-r-0 border-secondary py-2">
-                <div className="text-center space-x-3">
-                  <button className="cursor-pointer hover:opacity-60 transition ease-in-out duration-300">
-                    <GoCopy stroke="var(--color-secondary)" size={20} />
-                  </button>
-                  <button className="cursor-pointer hover:opacity-60 transition ease-in-out duration-300">
-                    <IoTrashBinOutline
-                      stroke="var(--color-secondary)"
-                      size={20}
-                    />
-                  </button>
-                </div>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
+      {children}
     </table>
   );
 };
+
+const Header: React.FC<PropsWithChildren> = ({ children }) => {
+  return <thead>{children}</thead>;
+};
+
+const Body: React.FC<PropsWithChildren> = ({ children }) => {
+  return <tbody>{children}</tbody>;
+};
+
+const Row: React.FC<PropsWithChildren<Props>> = ({ children, className }) => {
+  return <tr className={className}>{children}</tr>;
+};
+
+const Cell: React.FC<PropsWithChildren<Props>> = ({ children, className }) => {
+  return (
+    <td
+      className={twMerge(
+        "border-b px-4 border-r last:border-r-0 border-secondary py-2",
+        className
+      )}
+    >
+      {children}
+    </td>
+  );
+};
+interface TableComponent extends React.FC<PropsWithChildren> {
+  Header: React.FC<PropsWithChildren>;
+  Body: React.FC<PropsWithChildren>;
+  Row: React.FC<PropsWithChildren<Props>>;
+  Cell: React.FC<PropsWithChildren<Props>>;
+}
+
+const Table = TableWrapper as TableComponent;
+Table.Header = Header;
+Table.Body = Body;
+Table.Row = Row;
+Table.Cell = Cell;
 
 export default Table;
--- src/pages/trips/components/sections/Transport.tsx
+++ src/pages/trips/components/sections/Transport.tsx
@@ -1,22 +1,15 @@
 import { useState } from "react";
 import SmallButton from "../SmallButton";
 import Table from "../Table";
+import Checkbox from "../Checkbox";
+import { GoCopy } from "react-icons/go";
+import { IoTrashBinOutline } from "react-icons/io5";
 
 interface TransportProps {
   userCurrency?: string;
 }
 
 const Transport: React.FC<TransportProps> = ({ userCurrency }) => {
-  const headers = [
-    "name",
-    "total price",
-    "departure time",
-    "arrival time",
-    "from",
-    "to",
-    "",
-  ];
-
   const defaultComponent = {
     components: [
       <input type="text" className="focus:outline-0 w-full " />,
@@ -41,7 +34,58 @@ const Transport: React.FC<TransportProps> = ({ userCurrency }) => {
         + Add item
       </SmallButton>
       <div className="mt-5">
-        <Table headers={headers} data={data} checkedColor="bg-green" />
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.Cell>
+                <div className="flex items-center space-x-4 w-72">
+                  <Checkbox checked={true} />
+                  <span>name</span>
+                </div>
+              </Table.Cell>
+              <Table.Cell className="w-50">total price</Table.Cell>
+              <Table.Cell className="w-60">departure time</Table.Cell>
+              <Table.Cell className="w-60">arrival time</Table.Cell>
+              <Table.Cell className="w-50">from</Table.Cell>
+              <Table.Cell className="w-50">to</Table.Cell>
+              <Table.Cell className="w-30" />
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {data.map((row, index) => {
+              return (
+                <Table.Row key={index} className="group">
+                  <Table.Cell className="group-last:border-b-0">
+                    <div className="flex items-center space-x-4">
+                      <span>
+                        <Checkbox checked={row.checked} />
+                      </span>
+                      <span className="w-full">{row.components[0]}</span>
+                    </div>
+                  </Table.Cell>
+                  {row.components.slice(1).map((component, index) => (
+                    <Table.Cell key={index} className="group-last:border-b-0">
+                      {component}
+                    </Table.Cell>
+                  ))}
+                  <Table.Cell className="group-last:border-b-0">
+                    <div className="text-center space-x-3">
+                      <button className="cursor-pointer hover:opacity-60 transition ease-in-out duration-300">
+                        <GoCopy stroke="var(--color-secondary)" size={20} />
+                      </button>
+                      <button className="cursor-pointer hover:opacity-60 transition ease-in-out duration-300">
+                        <IoTrashBinOutline
+                          stroke="var(--color-secondary)"
+                          size={20}
+                        />
+                      </button>
+                    </div>
+                  </Table.Cell>
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table>
       </div>
     </div>
   );
