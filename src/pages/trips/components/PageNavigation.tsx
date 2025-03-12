e347b7c7744d7d9fc24f5e01a65f3fd1f3016832
Kaveesha Ranmuthu <kranmuthu1999@gmail.com>
2025-03-12 20:52:20 +1300

    dynamic font for page navigation

--- src/pages/trips/components/PageNavigation.tsx
+++ src/pages/trips/components/PageNavigation.tsx
@@ -1,6 +1,8 @@
 import { useClose } from "@headlessui/react";
 import React from "react";
 import { Link } from "react-scroll";
+import { useAuth } from "../../../hooks/useAuth";
+import { twMerge } from "tailwind-merge";
 
 interface PageNavigationProps {
   subCollections: string[];
@@ -8,9 +10,10 @@ interface PageNavigationProps {
 
 const PageNavigation: React.FC<PageNavigationProps> = ({ subCollections }) => {
   const close = useClose();
+  const { settings } = useAuth();
 
   return (
-    <nav className="space-y-2">
+    <nav className={twMerge("space-y-2", settings?.font)}>
       <h1 className="text-lg">Page navigation</h1>
       <ul className="space-y-1">
         {subCollections.map((collection) => (
