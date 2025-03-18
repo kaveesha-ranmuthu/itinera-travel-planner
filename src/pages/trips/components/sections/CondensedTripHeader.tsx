bfad02c9d90f8db7bcb9a48e6f4ca58fc5926ec7
Kaveesha Ranmuthu <kranmuthu1999@gmail.com>
2025-03-19 12:39:09 +1300

    add back button

--- src/pages/trips/components/sections/CondensedTripHeader.tsx
+++ src/pages/trips/components/sections/CondensedTripHeader.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { TripHeaderProps } from "./TripHeader";
+import { FaArrowLeft } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const CondensedTripHeader: React.FC<TripHeaderProps> = ({ trip }) => {
-  const { tripName, imageData } = trip;
+  const { tripName, imageData, id } = trip;
 
   return (
     <div className="bg-black rounded-3xl relative drop-shadow-(--drop-shadow-default)">
@@ -11,6 +13,12 @@ const CondensedTripHeader: React.FC<TripHeaderProps> = ({ trip }) => {
         alt={tripName}
         className="object-cover w-full h-[100px] rounded-3xl opacity-40"
       />
+      <Link
+        to={`/trip/${id}`}
+        className="absolute top-3 left-3 cursor-pointer z-40 hover:scale-105 transition ease-in-out duration-300"
+      >
+        <FaArrowLeft className="text-primary" size={20} />
+      </Link>
       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
         <span className="text-primary text-2xl w-[60%] text-center truncate py-5">
           {tripName}
