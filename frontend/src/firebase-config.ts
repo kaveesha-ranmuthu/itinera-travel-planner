b82ad8dbb22c333c5312c6fb53218cbe2352bc11
Kaveesha Ranmuthu <kranmuthu1999@gmail.com>
2025-03-26 23:06:18 +1300

    update delete function in frontend

--- frontend/src/firebase-config.ts
+++ frontend/src/firebase-config.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFunctions } from "firebase/functions";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,3 +22,6 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
+// ✅ Initialize Functions
+export const functions = getFunctions(app);
