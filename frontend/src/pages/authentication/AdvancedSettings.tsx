c61f2b847b88a858773910b2278e8dc119753687
Kaveesha <73220161+kaveesha-ranmuthu@users.noreply.github.com>
2025-06-05 20:35:01 +1200

    Delete user (#34)
    * add confirmation modal for deleting user
    
    * organise and create function to delete user
    
    * delete user on confirm

--- frontend/src/pages/authentication/AdvancedSettings.tsx
+++ frontend/src/pages/authentication/AdvancedSettings.tsx
@@ -11,13 +11,18 @@ import { twMerge } from "tailwind-merge";
 import DefaultPackingListEditor from "./components/DefaultPackingListEditor";
 import { useUpdateUserSettings } from "../trips/hooks/setters/useUpdateUserSettings";
 import { useHotToast } from "../../hooks/useHotToast";
-import { auth } from "../../firebase-config";
+import { auth, functions } from "../../firebase-config";
+import WarningConfirmationModal from "../trips/components/WarningConfirmationModal";
+import { httpsCallable } from "firebase/functions";
+import { signOut } from "firebase/auth";
 
 const AdvancedSettings = () => {
   const navigate = useNavigate();
   const { user, settings, setSettings } = useAuth();
   const { updateSettings } = useUpdateUserSettings();
   const { notify } = useHotToast();
+
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   const currentView = settings?.preferredDisplay || "gallery";
 
@@ -38,6 +43,19 @@ const AdvancedSettings = () => {
     }
   };
 
+  const deleteUser = async () => {
+    const deleteUserFn = httpsCallable(functions, "deleteUser");
+    try {
+      await deleteUserFn();
+      localStorage.clear();
+      await signOut(auth);
+      navigate("/login");
+    } catch (error) {
+      console.error("Failed to delete user:", error);
+      notify("Something went wrong. Please try again.", "error");
+    }
+  };
+
   return (
     <BackgroundWrapper>
       <BackArrow />
@@ -46,6 +64,15 @@ const AdvancedSettings = () => {
         <DefaultPackingListEditor
           open={isPackingListEditorOpen}
           onClose={() => setIsPackingListEditorOpen(false)}
+        />
+        <WarningConfirmationModal
+          font="font-brand italic tracking-wide"
+          hideIcon={true}
+          isOpen={isDeleteModalOpen}
+          onConfirm={deleteUser}
+          onClose={() => setIsDeleteModalOpen(false)}
+          title="Are you sure you want to delete your account?"
+          description="This action is permanent and cannot be undone. All of your saved trips and data will be permanently deleted."
         />
         <FormWrapper className="py-9">
           <h1 className="text-2xl mb-3 text-center">Settings</h1>
@@ -111,14 +138,15 @@ const AdvancedSettings = () => {
               <div className="space-x-2">
                 <Button.Primary
                   onClick={() => navigate("/reset-password")}
-                  type="submit"
+                  type="button"
                   className="mt-4 border border-secondary px-4 py-1 text-base transition ease-in-out duration-300"
                 >
                   Reset password
                 </Button.Primary>
                 <Button.Danger
-                  type="submit"
+                  type="button"
                   className="mt-4 px-4 py-1 text-base transition ease-in-out duration-300"
+                  onClick={() => setIsDeleteModalOpen(true)}
                 >
                   Delete account
                 </Button.Danger>
---
+++ functions/src/functions/deleteUser.ts
@@ -0,0 +1,26 @@
+import * as logger from "firebase-functions/logger";
+import { onCall } from "firebase-functions/v2/https";
+import { admin } from "../utils/admin";
+
+export const deleteUser = onCall(async (request) => {
+  const uid = request.auth?.uid;
+
+  if (!uid) {
+    logger.error("Unauthenticated request to deleteUser.");
+    throw new Error("User must be authenticated to delete their account.");
+  }
+
+  try {
+    // Delete Firestore user data recursively
+    const userDocRef = admin.firestore().doc(`users/${uid}`);
+    await admin.firestore().recursiveDelete(userDocRef);
+
+    await admin.auth().deleteUser(uid);
+
+    logger.info(`Successfully deleted user and data for UID: ${uid}`);
+    return { success: true };
+  } catch (error) {
+    logger.error("Error deleting user and data", error);
+    throw new Error("Failed to delete account. Please try again later.");
+  }
+});
